51adb68957e2b914da39bf6673d3230610310df7
freeman <1114509193@qq.com>
2024-08-15 09:59:45 +0800

    init

---
+++ src/Query/TestObject.java
@@ -0,0 +1,39 @@
+package Query;
+
+import java.util.List;
+
+/**
+ * TestObject
+ */
+public class TestObject {
+
+    private Integer a;
+    private Double b;
+    private String c;
+    private List<Integer> d;
+
+    public Integer getA() {
+        return a;
+    }
+    public void setA(Integer a) {
+        this.a = a;
+    }
+    public Double getB() {
+        return b;
+    }
+    public void setB(Double b) {
+        this.b = b;
+    }
+    public String getC() {
+        return c;
+    }
+    public void setC(String c) {
+        this.c = c;
+    }
+    public List<Integer> getD() {
+        return d;
+    }
+    public void setD(List<Integer> d) {
+        this.d = d;
+    }
+}
